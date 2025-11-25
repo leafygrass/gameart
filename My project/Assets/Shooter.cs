362641996178c70efa6348462e97a6ad0df44ed7
kelliekaw <kellie_kaw@dlsu.edu.ph>
2025-11-26 03:34:57 +0800

    feat: add projectile, vfx, and sfx

--- My project/Assets/Shooter.cs
+++ My project/Assets/Shooter.cs
@@ -33,7 +33,18 @@ public class Shooter : MonoBehaviour
             // Spawn point in front of player
             Vector3 spawnPos = transform.position + new Vector3(spawnOffset * direction, 0, 0);
             GameObject proj = Instantiate(projectile, spawnPos, Quaternion.identity);
-            proj.GetComponent<Rigidbody2D>().linearVelocity = new Vector2(direction * projectileSpeed, 0f);
+            // proj.GetComponent<Rigidbody2D>().linearVelocity = new Vector2(direction * projectileSpeed, 0f);
+            // Flip projectile sprite based on direction
+            Vector3 projScale = proj.transform.localScale;
+            projScale.x = Mathf.Abs(projScale.x) * direction;  // flip if direction is -1
+            proj.transform.localScale = projScale;
+
+            // Set projectile velocity
+            Rigidbody2D rb = proj.GetComponent<Rigidbody2D>();
+            if (rb != null)
+            {
+                rb.linearVelocity = new Vector2(direction * projectileSpeed, 0f);
+            }
         }
     }
 }
